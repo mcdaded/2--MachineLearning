d12f8115e48d7e7c42188ddae4d96a9b4aeff09c
danielmcdade <dmcdade@insightsquared.com>
2016-01-29 17:32:16 -0500

    octave

---
+++ Octave/Liner_Regression.m
@@ -0,0 +1,5 @@
+x = load('C:\Users\dmcdade\Documents\Python Scripts\100) github scripts\2--MachineLearning\Octave\ex2Data\ex2x.dat'); 
+y = load('C:\Users\dmcdade\Documents\Python Scripts\100) github scripts\2--MachineLearning\Octave\ex2Data\ex2y.dat');
+plot(x, y, 'o');
+ylabel('Height in meters')
+xlabel('Age in years')
